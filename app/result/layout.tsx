9a843e0aef0066cf20cdb217ad49ac9bb4ec671e
urassh <shuto.1011@icloud.com>
2024-07-16 14:07:04 +0000

    feat: 結果画面を追加

---
+++ app/result/layout.tsx
@@ -0,0 +1,7 @@
+export default function ResultLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <section>{children}</section>;
+}
---
+++ app/result/page.tsx
@@ -0,0 +1,8 @@
+export default function Result() {
+  return (
+    <main>
+      <h1>結果画面</h1>
+      <button>最初の画面に遷移</button>
+    </main>
+  );
+}
