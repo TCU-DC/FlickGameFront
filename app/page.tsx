ba3f4118213f6faeadb1b1b1c2309cd3cbc7fab1
urassh <shuto.1011@icloud.com>
2024-07-16 14:11:48 +0000

    feat: 最初の画面からPlay画面へと遷移させるようにした

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter();
+
+  const navigateToPlay = () => {
+    router.push("/play");
+  };
+
   return (
     <main>
       <h1>Flick Game!!</h1>
-      <button>Play</button>
+      <button onClick={navigateToPlay}>Play画面へ遷移</button>
     </main>
   );
 }
