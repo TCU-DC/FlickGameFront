4ad05a8abd5ea42e7ef27582c5839410d53a46ea
urassh <shuto.1011@icloud.com>
2024-07-16 14:15:06 +0000

    faet: Play画面からResult画面に遷移するようにした

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -1,8 +1,18 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function Play() {
+  const router = useRouter();
+
+  const navigateToResult = () => {
+    router.push("/result");
+  };
+
   return (
     <main>
       <h1>Play画面</h1>
-      <button>結果</button>
+      <button onClick={navigateToResult}>結果</button>
     </main>
   );
 }
