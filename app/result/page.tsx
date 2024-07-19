eb83610bc7c41165f7869a792ee74d5f4bae1784
urassh <shuto.1011@icloud.com>
2024-07-19 12:29:07 +0000

    feat: result画面にスコアが表示されるようになった

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -1,9 +1,14 @@
+"use client";
+
 import NavigateButton from "@/components/NavigateButton";
 
 export default function Result() {
+  const score = localStorage.getItem("score");
+
   return (
     <main>
       <h1>Result画面</h1>
+      <p>あなたのスコア : {score ? score : 0}点です</p>
       <NavigateButton to="" label="最初の画面へ" />
     </main>
   );
