f3e15038a2772ff7e688533f16521731533e9e3f
urassh <shuto.1011@icloud.com>
2024-07-20 10:50:17 +0000

    add: ScorePoster

---
+++ sender/ScorePoster.ts
@@ -0,0 +1,19 @@
+import { DataPoster } from "@/interfaces/DataPoster";
+import { ScoreRequest } from "@/models/scoreRequest";
+
+export class ScorePoster implements DataPoster<ScoreRequest> {
+  private readonly BASE_URL = process.env.BASE_URL;
+  private readonly ENDPOINT = "/register-score";
+  private readonly URL = `${this.BASE_URL}${this.ENDPOINT}`;
+
+  async post(data: ScoreRequest): Promise<Response> {
+    const response = await fetch(this.URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    return response;
+  }
+}
