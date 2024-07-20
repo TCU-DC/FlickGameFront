aae0a39eb48030b5ad8e5024b2eb01bb879bb17e
urassh <shuto.1011@icloud.com>
2024-07-20 18:48:18 +0000

    fix: LEVELをeasyに変更

--- loaders/WordLoaderFromApi.ts
+++ loaders/WordLoaderFromApi.ts
@@ -5,7 +5,7 @@ export class WordLoaderFromApi implements DataLoader<WordListResponse> {
   private readonly BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
   private readonly ENDPOINT = "/word-get";
   private readonly URL = new URL(`${this.BASE_URL}${this.ENDPOINT}`);
-  private readonly LEVEL = "normal";
+  private readonly LEVEL = "easy";
   private readonly COUNT = "10";
 
   constructor() {
@@ -14,6 +14,8 @@ export class WordLoaderFromApi implements DataLoader<WordListResponse> {
   }
 
   async load(): Promise<WordListResponse> {
+    console.log(this.BASE_URL);
+
     const response = await fetch(this.URL);
     const data: WordListResponse = await response.json();
 
