754a449b5186ff48ee9b046297903b380134b69a
urassh <shuto.1011@icloud.com>
2024-07-20 18:32:37 +0000

    feat: apiからwordのデータを取得する処理を実装した

---
+++ loaders/WordLoaderFromApi.ts
@@ -0,0 +1,24 @@
+import { DataLoader } from "@/interfaces/DataLoader";
+import { WordListResponse } from "@/models/word";
+
+export class WordLoaderFromApi implements DataLoader<WordListResponse> {
+  private readonly BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+  private readonly ENDPOINT = "/word-get";
+  private readonly URL = new URL(`${this.BASE_URL}${this.ENDPOINT}`);
+  private readonly LEVEL = "normal";
+  private readonly COUNT = "10";
+
+  constructor() {
+    this.URL.searchParams.append("level", this.LEVEL);
+    this.URL.searchParams.append("count", this.COUNT);
+  }
+
+  async load(): Promise<WordListResponse> {
+    const response = await fetch(this.URL);
+    const data: WordListResponse = await response.json();
+
+    console.log("WordLoaderFromApi", data);
+
+    return data;
+  }
+}
