619f245c4abb924e19830e33036519c202a13e50
urassh <shuto.1011@icloud.com>
2024-07-20 19:15:49 +0000

    feat: RankingLoaderFromApiを実装

---
+++ loaders/RankingLoaderFromApi.ts
@@ -0,0 +1,24 @@
+import { DataLoader } from "@/interfaces/DataLoader";
+import { RankingResponse } from "@/models/ranking";
+
+export class RankingLoaderFromApi implements DataLoader<RankingResponse> {
+  private readonly BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+  private readonly ENDPOINT = "/get-ranking";
+  private readonly URL = new URL(`${this.BASE_URL}${this.ENDPOINT}`);
+  private readonly LEVEL = "easy";
+  private readonly HIGH_ORDER = "3";
+
+  constructor() {
+    this.URL.searchParams.append("level", this.LEVEL);
+    this.URL.searchParams.append("high_order", this.HIGH_ORDER);
+  }
+
+  async load(): Promise<RankingResponse> {
+    console.log(this.BASE_URL);
+
+    const response = await fetch(this.URL);
+    const data: RankingResponse = await response.json();
+
+    return data;
+  }
+}
