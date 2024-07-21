adee51abe042a50dc7fe45a8fc492a5a3bc845f1
urassh <shuto.1011@icloud.com>
2024-07-21 04:37:11 +0000

    feat: websocket resultの実装

--- components/Result.tsx
+++ components/Result.tsx
@@ -1,34 +1,98 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { RankingResponse } from "@/models/ranking";
 import NavigateButton from "@/components/NavigateButton";
+import { RoomAction } from "@/interfaces/RoomAction";
+import { ResultSocketInfo } from "@/interfaces/ResultSocketInfo";
 
 type ResultProps = {
   response: RankingResponse;
 };
 
 export const Result = ({ response }: ResultProps) => {
-  const [loading, setLoading] = useState(true);
   const [score, setScore] = useState(0);
-  useEffect(() => {
-    const getItem = localStorage.getItem("score");
-    if (getItem) {
-      setScore(parseInt(getItem));
-    }
-  }, []);
-
-  const level = response.level;
-  const ranking = response.ranking;
+  const [resultSockerInfo, setResultSockerInfo] = useState<ResultSocketInfo>();
+  const [connected, setConnected] = useState(false);
+  const [roomMembersResult, setRoomMembersResult] = useState<
+    ResultSocketInfo[]
+  >([]);
+  const webSocketRef = useRef<WebSocket>();
 
   useEffect(() => {
-    if (response) {
-      setLoading(false);
-    }
-  }, [response]);
+    const getLocalScore = localStorage.getItem("score");
+    const getLocalInfo = localStorage.getItem("joinInfo");
 
-  if (loading) {
-    return <div>Loading...</div>;
+    if (getLocalScore) {
+      setScore(parseInt(getLocalScore));
+    }
+
+    if (getLocalInfo) {
+      const parsedJoinInfo: RoomAction = JSON.parse(getLocalInfo);
+
+      const resultSocketInfo: ResultSocketInfo = {
+        room: parsedJoinInfo.room,
+        nickname: parsedJoinInfo.nickname,
+        member_type: parsedJoinInfo.member_type,
+        action: parsedJoinInfo.action,
+        score: getLocalScore || "0",
+      };
+
+      setResultSockerInfo(resultSocketInfo);
+
+      const ws = new WebSocket(
+        `${process.env.NEXT_PUBLIC_WEB_SOCKET_BASE_URL}/room-result/${resultSocketInfo.room}`,
+      );
+
+      ws.onopen = () => {
+        console.log("connected");
+        setConnected(true);
+      };
+
+      ws.onclose = () => {
+        console.log("WebSocket closed");
+        setConnected(false);
+      };
+
+      webSocketRef.current = ws;
+      webSocketRef.current.onmessage = (event) => {
+        const data: ResultSocketInfo = JSON.parse(event.data);
+        setRoomMembersResult([...roomMembersResult, data]);
+      };
+      return () => ws.close();
+    }
+  }, [roomMembersResult]);
+
+  useEffect(() => {
+    const successConnection = connected && resultSockerInfo;
+    const wsIsOpen =
+      webSocketRef.current &&
+      webSocketRef.current.readyState === WebSocket.OPEN;
+
+    if (!successConnection) return;
+    if (!wsIsOpen) {
+      console.error("WebSocket is not open");
+      return;
+    }
+
+    webSocketRef.current!.send(JSON.stringify(resultSockerInfo));
+  }, [connected, resultSockerInfo]);
+
+  let level: string = "";
+  let ranking: { nickname: string; score: number }[] = [];
+
+  if (roomMembersResult.length === 0) {
+    level = response.level;
+    ranking = response.ranking;
+  } else {
+    ranking = roomMembersResult.map((member) => {
+      return {
+        nickname: member.nickname,
+        score: parseInt(member.score),
+      };
+    });
+
+    ranking.sort((a, b) => b.score - a.score);
   }
 
   return (
